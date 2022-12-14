de137648575310f135c0c4acac9c29c679b9c698
Danil Kraynov <frostmorn712@gmail.com>
2022-12-14 23:21:14 +0300

    Hello world commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val ktorVersion: String by project
+val muVersion: String by project
+val logbackVersion: String by project
+
 plugins {
     kotlin("jvm") version "1.7.20"
     application
@@ -14,6 +18,17 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-cio:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-logging:$ktorVersion")
+    implementation("io.github.microutils:kotlin-logging-jvm:$muVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+}
+
+application {
+    mainClass.set("com.timetable.ApplicationKt")
 }
 
 tasks.test {
@@ -22,8 +37,4 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
-}
-
-application {
-    mainClass.set("MainKt")
 }
---
+++ src/main/kotlin/com/timetable/Application.kt
@@ -0,0 +1,30 @@
+package com.timetable
+
+import com.timetable.routing.configureHelloWorldRouting
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.cio.*
+import io.ktor.server.engine.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.contentnegotiation.*
+
+fun main() {
+    val env = applicationEngineEnvironment {
+        module {
+            connector {
+                port = 8080
+            }
+
+            install(CallLogging)
+            install(ContentNegotiation) {
+                json()
+            }
+
+            configureHelloWorldRouting()
+        }
+    }
+
+    embeddedServer(CIO, env).apply {
+        start(true)
+    }
+}
