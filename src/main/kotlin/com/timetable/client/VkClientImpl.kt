b8331c77656d0eab17a540cd193c388a68f4887b
Danil Kraynov <frostmorn712@gmail.com>
2022-12-19 01:06:01 +0300

    vk-bot 1.3.0: changed access key

--- src/main/kotlin/com/timetable/client/VkClientImpl.kt
+++ src/main/kotlin/com/timetable/client/VkClientImpl.kt
@@ -21,6 +21,6 @@ class VkClientImpl(
 
     companion object {
         private const val GROUP_ID = "217746000"
-        private const val ACCESS_TOKEN = "vk1.a.LM2U58gxDgJX8I55_PIg6ZfmrSewYdZ1ghYzlh9ulm4mSEqZld5RHHYtTFbOvja2pQQRsDBbzb3DIitTBpxffoDo8oC5YV-nZMTBv1XbHnQJI-XbuhNif7dTrlT_rVCcBJgVPgIkk31HNYhRVS-O4CW_VwP9VbE191uj5xji8Jzv12TUliqUcHxafCcl6KkqtR2RbJgGHzxNLQIZtDc44w"
+        private val ACCESS_TOKEN = System.getenv("VK_KEY") ?: "stub"
     }
 }
